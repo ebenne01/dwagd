64539e4c5bdaba692ad0ae0ebeaf62d00c4be2c1
Edward Bennett <44945949+ebenne01@users.noreply.github.com>
2019-09-16 22:45:58 -0400

    Initial Commit

---
+++ DayOfWeek/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  DayOfWeek
+//
+//  Created by Edward Bennett on 9/16/19.
+//  Copyright © 2019 Edward Bennett. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
