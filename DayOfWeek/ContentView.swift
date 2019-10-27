8b22c52c764ec0740c073ee734905aa10c74cc78
Edward Bennett <44945949+ebenne01@users.noreply.github.com>
2019-10-27 00:14:56 -0400

    Layout UI

--- DayOfWeek/ContentView.swift
+++ DayOfWeek/ContentView.swift
@@ -9,15 +9,42 @@
 import SwiftUI
 
 struct ContentView : View {
-    var body: some View {
-        Text("Hello World")
+  @State var textValue = ""
+  @State var textFieldValue = ""
+  
+  var body: some View {
+    Form {
+      Section(header: Text("Day of Week")) {
+        TextField("Date (mm/dd/yyyy)", text: $textFieldValue) {
+        }
+        Text(textValue)
+      }
+      
+      Section {
+        HStack {
+          Spacer()
+          Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+            Text("Calculate Day")
+          }
+          Spacer()
+        }
+        HStack {
+          Spacer()
+          Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+            Text("Clear")
+          }
+          Spacer()
+        }
+      }
     }
+  }
 }
+
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
 #endif
